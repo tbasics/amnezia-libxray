ec9ac9d317daff5c8e070780f47597dfff3bc232
albexk <albexk@proton.me>
2024-06-10 21:25:16 +0300

    Fix Android assets path

--- xray_wrapper_android.go
+++ xray_wrapper_android.go
@@ -10,11 +10,14 @@ import (
 )
 
 // Prepare Xray for android
-func InitXray() {
+func InitXray(assetsPath string) {
 	xrayfs.NewFileReader = func(path string) (io.ReadCloser, error) {
 		if _, err := os.Stat(path); os.IsNotExist(err) {
-			_, file := filepath.Split(path)
-			return mobasset.Open(file)
+			asset, err := filepath.Rel(assetsPath, path)
+			if err != nil {
+				return nil, err
+			}
+			return mobasset.Open(asset)
 		}
 		return os.Open(path)
 	}
